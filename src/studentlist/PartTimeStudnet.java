47d70b423dc3ffa56a992dc07be12875697a50be
makkaarm <makkaarm@Makkar>
2024-09-16 10:38:53 -0400

    num courses

---
+++ src/studentlist/PartTimeStudnet.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author makkaarm
+ */
+public class PartTimeStudnet extends Student {
+        private int NumCourses;
+
+    public int getNumCourses() {
+        return NumCourses;
+    }
+
+    public void setNumCourses(int NumCourses) {
+        this.NumCourses = NumCourses;
+    }
+    
+}
